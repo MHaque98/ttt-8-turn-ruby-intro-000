1c6dc6384bb47707adeab04b5c3235cc509ff891
Mahmud H <mahmud_haque@hotmail.co.uk>
2020-08-05 14:11:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,14 +28,3 @@ end
 def move(board, position, value = "X")
   board[position] = value
 end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    #fd
-  else
-    puts "Please try again"
-  end
-end
