060104ccb28a97efe689b5323226117fefc97785
Mahmud H <mahmud_haque@hotmail.co.uk>
2020-08-05 14:15:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,8 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
+  index = input
+  input_to_index(index)
   if valid_move?(board, index)
     #fd
   else
